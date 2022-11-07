88c1d37e49591709d2258613b33595b51778c070
martinkovalcik <koviyolo@gmail.com>
2022-11-07 16:57:02 +0100

    starting

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyA3PU-SbOMHDVYoyviUEpFt2hjY5G_rklc",
+    authDomain: "smart-greenhouse-a7f8f.firebaseapp.com",
+    projectId: "smart-greenhouse-a7f8f",
+    storageBucket: "smart-greenhouse-a7f8f.appspot.com",
+    messagingSenderId: "742790391084",
+    appId: "1:742790391084:web:4eb0070c5c640c5ae63889"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
