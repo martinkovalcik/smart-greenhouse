dc078e9b6fb4d353723240cc54b71737080590db
Martin Kovi Kovalčík <41521947+martinkovalcik@users.noreply.github.com>
2023-02-10 10:31:59 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,5 @@
 export const environment = {
   production: true,
-  weatherApiBaseUrl:'https://open-weather13.p.rapidapi.com/city',
-  XRapidAPIHostHeaderName:'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue:'open-weather13.p.rapidapi.com',
-  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue:'2de3da4fd0msh20b46a1e8b6ec06p185df8jsnd2bde0530575',
   firebase : {
     apiKey: "AIzaSyA3PU-SbOMHDVYoyviUEpFt2hjY5G_rklc",
     authDomain: "smart-greenhouse-a7f8f.firebaseapp.com",
